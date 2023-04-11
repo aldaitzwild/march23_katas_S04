7eb109969d90bfbdbddff51ec86508f3dbccfe44
Thomas Aldaitz <thomas.aldaitz@gmail.com>
2023-04-11 08:51:25 +0200

    Add the kata for tuesday

---
+++ tuesday.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * en vous servant du tableau suivant, trier les membre par nom 
+ * et afficher le résultat suivant :
+ * 
+    Donatello a le bandeau violet
+    Leonardo a le bandeau bleu
+    Michelangleo a le bandeau orange
+    Raphael a le bandeau rouge 
+ */
+
+ $tmnts = [
+    ['name' => 'Leonardo', 'color' => 'bleu'],
+    ['name' => 'Michelangleo', 'color' => 'orange'],
+    ['name' => 'Donatello', 'color' => 'violet'],
+    ['name' => 'Raphael', 'color' => 'rouge'],
+ ];
