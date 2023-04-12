9de8ddde1af3d7c56a66e8feb79625c271d5aea9
Thomas Aldaitz <thomas.aldaitz@gmail.com>
2023-04-12 08:54:07 +0200

    Add Wednesday

---
+++ wednesday.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * A partir d'un tableau contenant des chiffres, en créer un second à deux dimensions
+ * avec autant de case que mentionné dans le premier tableau
+ * 
+ * [4][2][8][3][5]
+ * 
+ * ==>
+ * [4][4][4][4]
+ * [2][2]
+ * [8][8][8][8][8][8][8][8]
+ * [3][3][3]
+ * [5][5][5][5][5]
+ * 
+ * puis afficher avec un print_r()
+ */
+
+ $values = [4, 2, 8, 3, 5];
